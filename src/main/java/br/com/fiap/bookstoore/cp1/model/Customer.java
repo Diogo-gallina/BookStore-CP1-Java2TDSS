0825014da25b10bde147c7bfb962a04c5fc550e6
diogo-gallina <diogogallina20@gmail.com>
2024-03-27 22:37:22 -0300

    feat: create customer model

---
+++ src/main/java/br/com/fiap/bookstoore/cp1/model/Customer.java
@@ -0,0 +1,47 @@
+package br.com.fiap.bookstoore.cp1.model;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name="JAVA-CP1-CUSTOMER")
+@EntityListeners(AuditingEntityListener.class)
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "customer_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "email", nullable = false, length = 70)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 40)
+    private String password;
+
+    @Column(name = "birthday_date", nullable = false)
+    private LocalDate birthdayDate;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    //TODO: Relacionar ManyToMany com Book
+}
