cb7ddcbcdca639f0f46158e494488691ef297d87
diogo-gallina <diogogallina20@gmail.com>
2024-03-28 11:46:35 -0300

    feat: create adress entity

---
+++ src/main/java/br/com/fiap/bookstoore/cp1/model/Adress.java
@@ -0,0 +1,42 @@
+package br.com.fiap.bookstoore.cp1.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name = "JAVA-CP1-ADRESS")
+@EntityListeners(AuditingEntityListener.class)
+public class Adress {
+
+    @Id
+    @Column(name = "adress_id")
+    private Long id;
+
+    @Column(name = "cep", nullable = false, length = 11)
+    private String cep;
+
+    @Column(name = "street", nullable = false, length = 70)
+    private String street;
+
+    @Column(name = "number", nullable = false)
+    private int number;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    //TODO: Adicionar relacionamento com Cliente
+
+}
